9b0bd8e9421310fddaa19005142e6265054af6d3
Thomas Ha <thomasha.1310@gmail.com>
2025-07-03 14:39:41 -0400

    Add unit test with assert

--- labs/lab-3-data-parsing/src/string_utils.py
+++ labs/lab-3-data-parsing/src/string_utils.py
@@ -10,3 +10,10 @@ def clean_strings(strings: pandas.Series) -> pandas.Series:
     pattern = f"[{re.escape(string.punctuation)}]" # regex pattern for punctuation
     strings = strings.str.replace(pattern, '', regex=True)
     return strings
+
+df = pandas.DataFrame(columns=['raw', 'clean'])
+df['raw'] = pandas.read_csv('../data/messy_strings.csv')
+df['clean'] = clean_strings(df['raw'])
+
+assert list(df['clean'].value_counts()) == [12, 11, 9, 9, 5, 4]
+print('Outputs matched expected values.')
