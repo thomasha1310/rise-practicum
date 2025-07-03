9aa4feea4da2a77d8ff250f51d5e52d228fe77fc
Thomas Ha <thomasha.1310@gmail.com>
2025-07-03 12:50:49 -0400

    Move clean_strings method into string_utils.py

---
+++ labs/lab-3-data-parsing/src/string_utils.py
@@ -0,0 +1,8 @@
+def clean_strings(strings: pandas.Series) -> pandas.Series:
+    strings = strings.dropna()
+    strings = strings.astype(str)
+    strings = strings.str.lower()
+    strings = strings.str.strip()
+    pattern = f"[{re.escape(string.punctuation)}]" # regex pattern for punctuation
+    strings = strings.str.replace(pattern, '', regex=True)
+    return strings
